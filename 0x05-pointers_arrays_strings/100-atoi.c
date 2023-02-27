343d8ef41a520b01052ee8417af91ab6e64f9512
ClaraChagonda08 <122863782+ClaraChagonda08@users.noreply.github.com>
2023-02-27 15:00:30 +0200

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
 /**
-* _atoi - Converts a string to an integer.
-* @s: The string to be converted.
-*
-* Return: The integer value of the converted string.
-*/
+ * _atoi - return a num inside of a string
+ *
+ * @s: pointer for string to parse
+ * Return: int pulled from string, 0 if none
+ */
 int _atoi(char *s)
 {
-	int sign = 1;
 	unsigned int num = 0;
+	int flag = -1, i = 0, neg = 1;
 
-	do {
-		if (*s == '-')
-			sign *= -1;
-
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
-			break;
-
-	} while (*s++);
-
-	return (num * sign);
+	if (*s == '\0')
+		flag = 1;
+	for (i = 0; flag == -1; i++)
+	{
+		if ((*(s + i) >= '0') && (*(s + i) <= '9'))
+		{
+			if (num != 0)
+				num = ((num * 10) + (*(s + i) - '0'));
+			else
+				num = (*(s + i) - '0');
+		}
+		if (*(s + i + 1) == '\0')
+			flag = 1;
+		if (*(s + i) == '-')
+			neg *= -1;
+		if (num != 0)
+		{
+			if ((*(s + i + 1) < '0') || (*(s + i + 1) > '9'))
+				flag = 0;
+		}
+	}
+	return (neg * num);
 }
