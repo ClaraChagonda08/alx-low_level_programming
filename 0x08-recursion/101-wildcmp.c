3367d9953fa041d26debf23e26fd752121da3ea1
ClaraChagonda08 <122863782+ClaraChagonda08@users.noreply.github.com>
2023-03-09 21:38:11 +0200

    wild

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,38 +1,40 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code
+ * wildcmp - compares two string with a * asterisk exception
  *
- * Return: Always 0.
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if identical, 0 if not
  */
-int main(void)
+int wildcmp(char *s1, char *s2)
 {
-    int r;
-
-    r = wildcmp("main.c", "*.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*a*i*n*.*c*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "main.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "ma********************************c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "***");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m.*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "**.*c");
-    printf("%d\n", r);
-    r = wildcmp("main-main.c", "ma*in.c");
-    printf("%d\n", r);
-    r = wildcmp("main", "main*d");
-    printf("%d\n", r);
-    r = wildcmp("abc", "*b");
-    printf("%d\n", r);
-    return (0);
+	if (s1 == NULL && s2 == NULL)
+		return (1);
+	else if (s1 == NULL || s2 == NULL)
+		return (0);
+	if (*s1 == '\0')
+	{
+		if (*s2 == '*' && *(s2 + 1) == '\0')
+			return (1);
+		if (*s1 == *s2)
+			return (1);
+		else
+			return (0);
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (*s1 == *(s2 + 1))
+		{
+			if ((wildcmp(s1, s2 + 1)) == 1)
+				return (1);
+		}
+		return (wildcmp(s1 + 1, s2));
+	}
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
