6a41eb207864c98e7057856097a5ad9963069498
ClaraChagonda08 <122863782+ClaraChagonda08@users.noreply.github.com>
2023-03-16 12:00:05 +0200

    malloc free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/**
+ * program that multiplies two positive numbers.
+ * Usage: mul num1 num2
+ * Print the result, followed by a new line
+ * If the number of arguments is incorrect, print Error, followed by a new line, and exit with a status of 98
+ * num1 and num2 should only be composed of digits. If not, print Error, followed by a new line, and exit with a status of 98
+ * num1 and num2 will be passed in base 10
+ * Print the result, followed by a new line
+ * Return: Product.
+ */
+void print_product(void)
+{
+	int num1;
+	int num2;
+	int multi;
+
+	
+}
