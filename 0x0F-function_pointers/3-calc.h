3a1dfae11c36198b85c3c839c8cb1116f2274699
ClaraChagonda08 <122863782+ClaraChagonda08@users.noreply.github.com>
2023-03-22 15:16:05 +0200

    calc

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,12 @@
-#ifndef OPERATIONS
-#define OPERATIONS
+ifndef _CALC_H_
+#define _CALC_H_
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
  * struct op - Struct op
  *
@@ -10,11 +17,6 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}	op_t;
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-#endif
+} op_t;
+
+#endif /*_CALC_H_*/
