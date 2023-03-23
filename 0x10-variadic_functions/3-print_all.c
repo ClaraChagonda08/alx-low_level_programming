8906bb2f6b969b801a6dd538ff84d97b5a73cde4
ClaraChagonda08 <122863782+ClaraChagonda08@users.noreply.github.com>
2023-03-23 14:57:16 +0200

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,82 +1,97 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
-/**
- * print_i - prints int
- * @list: arguement of list
- * @s: seperator
- * Return: none
- */
-void print_i(va_list list, char *s)
-{
-	printf("%s%d", s, va_arg(list, int));
-}
-/**
- * print_c - prints char
- * @list: arguement char
- * @sep: seperator
- */
-void print_c(va_list list, char *sep)
-{
-	printf("%s%c", sep, va_arg(list, int));
-}
-/**
- * print_s - prints string
- * @sep: seperator
- * @list: list to print
- * Return: none
- */
-void print_s(va_list list, char *sep)
-{
-	char *s;
 
-	s = va_arg(list, char *);
-	if (s == NULL)
-		s = "(nil)";
-	printf("%s%s", sep, s);
-}
 /**
- * print_f - prints floats
- * @sep: float to print
- * @list: next arguement of list to print
- * Return: none
+ * print_string - print strings
+ *
+ * @list: list of arguments
+ * Return: void
  */
-void print_f(va_list list, char *sep)
+void print_string(va_list list)
 {
-	printf("%s%f", sep, va_arg(list, double));
+	char *temp;
+
+	temp = va_arg(list, char *);
+	if (temp == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", temp);
 }
+
 /**
- * print_all - prints out all stuff
- * @format: format is list of types of arguements
+ * print_char - print a char
+ *
+ * @list: list of arguments
+ * Return: void
+ */
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * print_int - prints an int
+ *
+ * @list: list of arguments
+ * Return: void
+ */
+void print_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_float - prints a float
+ *
+ * @list: list of arguments
+ * Return: void
+ */
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * print_all - prints anything based on passed format string
+ *
+ * @format: constant that tells what type of format to print
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
 	va_list list;
-	char *sep;
-	int i, j;
-	fm_t fm[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{"s", print_s},
-		{NULL, NULL}
+	type_print typs[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
 	};
+	int i, k;
+	char *separator;
+
 	va_start(list, format);
 	i = 0;
-	sep = "";
-	while (format != NULL && format[i] != '\0')
+	k = 0;
+	separator = "";
+	while (format != NULL && *(format + k) != '\0')
 	{
-		j = 0;
-		while (j < 4)
+		while (typs[i].type_name && typs[i].t != *(format + k))
 		{
-			if (format[i] == *(fm[j]).fm)
-			{
-				fm[j].p(list, sep);
-				sep = ", ";
-			}
-			j++;
+			i++;
 		}
-		i++;
+		if (typs[i].type_name != NULL)
+		{
+			printf("%s", separator);
+			typs[i].type_name(list);
+			separator = ", ";
+		}
+		k++;
+		i = 0;
 	}
 	printf("\n");
 	va_end(list);
